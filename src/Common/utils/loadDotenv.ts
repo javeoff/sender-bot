dec7e957ff50e02bf38f1f2bae375dfbc5c07cdc
Dmitry Fuks <difuks@gmail.com>
2022-09-15 21:55:00 +0300

    fix dot env

--- src/Common/utils/loadDotenv.ts
+++ src/Common/utils/loadDotenv.ts
@@ -1,5 +1,7 @@
+import path from 'path';
+
 export const loadDotenv = () => {
   require('dotenv').config({
-    path: `.env${process.env.NODE_ENV !== 'production' ? '.local' : ''}`
+    path: path.join(process.cwd(), `.env${process.env.NODE_ENV !== 'production' ? '.local' : ''}`),
   })
 }
